f23eed3a1053b8eb119636f6a681051d6d73d993
neat159 <ertu413@gmail.com>
2014-09-06 11:26:04 +0300

    added negativeData.xml and testBreak()

--- src/test/java/com/mebitech/lightsch/validator/SchematronValidatorTest.java
+++ src/test/java/com/mebitech/lightsch/validator/SchematronValidatorTest.java
@@ -10,27 +10,42 @@ import java.util.List;
 
 public class SchematronValidatorTest {
 
-	@Test
-	public void testValidate() throws Exception {
-		Schematron schematron = SchematronParser.parse(SchematronValidatorTest.class.getClassLoader().getResourceAsStream("testSch.xml"));
-		URL xmlPath = SchematronValidatorTest.class.getClassLoader().getResource("positiveData.xml");
-		List<Assert> failed = SchematronValidator.validate(schematron, xmlPath);
-		assert  failed.isEmpty();
+    @Test
+    public void testValidate() throws Exception {
+        Schematron schematron = SchematronParser.parse(SchematronValidatorTest.class.getClassLoader().getResourceAsStream("testSch.xml"));
+        URL xmlPath = SchematronValidatorTest.class.getClassLoader().getResource("positiveData.xml");
+        List<Assert> failed = SchematronValidator.validate(schematron, xmlPath);
+        assert failed.isEmpty();
 
 
-	}
+    }
+
+    @Test
+    public void testBreak() throws Exception {
+
+        Schematron schematron = SchematronParser.parse(SchematronValidatorTest.class.getClassLoader().getResourceAsStream("testSch.xml"));
+        URL xmlPath = SchematronValidatorTest.class.getClassLoader().getResource("negativeData.xml");
+        List<Assert> failed = SchematronValidator.validate(schematron, xmlPath);
+
+        System.out.println("\n\n*******************************************************************");
+        System.out.println("Failed Size: " + failed.size());
+        System.out.println("*******************************************************************\n\n");
+
+        assert failed.isEmpty();
+
+    }
 
 
-	@Test
-	public void testPathFunctionRemover() {
-		String xpath = "not(gl-cor:account/normalize-space(gl-cor:accountMainID)) or not(gl-cor:account/normalize-space(gl-cor:accountSub/gl-cor:accountSubID)) or starts-with(gl-cor:account/normalize-space(gl-cor:accountSub/gl-cor:accountSubID), gl-cor:account/normalize-space(gl-cor:accountMainID))";
-		String replaced = "not(not(gl-cor:account/gl-cor:accountMainID) or not(gl-cor:account/gl-cor:accountSub/gl-cor:accountSubID) or starts-with(gl-cor:account/gl-cor:accountSub/gl-cor:accountSubID, gl-cor:account/gl-cor:accountMainID))";
-		Assert anAssert = new Assert(xpath);
+    @Test
+    public void testPathFunctionRemover() {
+        String xpath = "not(gl-cor:account/normalize-space(gl-cor:accountMainID)) or not(gl-cor:account/normalize-space(gl-cor:accountSub/gl-cor:accountSubID)) or starts-with(gl-cor:account/normalize-space(gl-cor:accountSub/gl-cor:accountSubID), gl-cor:account/normalize-space(gl-cor:accountMainID))";
+        String replaced = "not(not(gl-cor:account/gl-cor:accountMainID) or not(gl-cor:account/gl-cor:accountSub/gl-cor:accountSubID) or starts-with(gl-cor:account/gl-cor:accountSub/gl-cor:accountSubID, gl-cor:account/gl-cor:accountMainID))";
+        Assert anAssert = new Assert(xpath);
 
-		System.out.println(replaced);
-		System.out.println(anAssert.getTest());
-		assert anAssert.getTest().equals(replaced);
+        System.out.println(replaced);
+        System.out.println(anAssert.getTest());
+        assert anAssert.getTest().equals(replaced);
 
 
-	}
+    }
 }
